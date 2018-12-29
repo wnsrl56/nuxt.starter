aab7f6453694c6dc678b2cc3e2b2cbac461a4fc5
Jun <wnsrl56@gmail.com>
2018-12-29 15:05:55 +0900

    [#1] - Article Component 추가
    Main App 구성을 위한 Proto Component 추가

---
+++ components/Article.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <h1> Hello, {{ text }}</h1>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      text : {
+        type: String,
+        default: '435345ㅈ'
+      },
+    },
+    created() {
+      console.log('test')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
-  <p>{{ $data }}</p>
+  <div>
+    <Article text="World!" />
+  </div>
 </template>
 <script>
-  export default {
-      data() {
-        return {
-            number: 1,
-        }
-      },
-      methods: {
+  import Article from "../components/Article"
 
+  export default {
+    components: {
+      Article,
+    },
+    data() {
+      return {
       }
+    },
+    methods: {
+    }
   }
 </script>
 <style type="scss">
-</style>qw
+</style>
